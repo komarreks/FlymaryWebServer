c43729505247f9e75fe00e68d472ecb568bc7a6a
Sergey Komarov <skomar-off@yandex.ru>
2024-08-31 00:31:42 +0500

    31.08.2024 поправлены методы работы с заказами на ключи с UUID

--- src/main/java/main/transfer/OrdersController.java
+++ src/main/java/main/transfer/OrdersController.java
@@ -24,6 +24,7 @@ import org.springframework.web.bind.annotation.*;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/orders")
@@ -100,7 +101,9 @@ public class OrdersController {
     public ResponseEntity createOrder(@RequestBody JsonNode jsUserId){
         Order order = Order.createNewOrder();
 
-        //order.setUser(userRepository.findById(jsUserId.get("userId").intValue()).get());
+        String stringUserId = jsUserId.get("userId").textValue();
+
+        order.setUser(userRepository.findById(UUID.fromString(stringUserId)).get());
 
         orderRepository.save(order);
 
@@ -109,80 +112,80 @@ public class OrdersController {
 
     @PostMapping(value = "/addLine", consumes = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity addLine(@RequestBody JsonNode jsLine){
-//        Order order = orderRepository.findById(jsLine.get("id").longValue());
-//
-//        if (order == null){return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new SimpleAnswer("not found order"));}
-//
-//        Product product = productReposytory.findById(jsLine.get("productId").longValue());
-//
-//        if (product == null){return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new SimpleAnswer("not found product"));}
-//
-//        long characId = jsLine.get("characId").longValue();
-//
-//        Charac charac = null;
-//
-//        if (characId > 0){
-//            charac = characRepository.findById(characId);
-//
-//            if (charac == null){return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new SimpleAnswer("not found charac"));}
-//        }
-//
-//        int count = jsLine.get("count").intValue();
-//        double price = jsLine.get("price").doubleValue();
-//
-//        order.addLine(product, charac, count, price);
-//
-//        orderRepository.save(order);
-//
+        Order order = orderRepository.findById(UUID.fromString(jsLine.get("id").textValue())).get();
+
+        if (order == null){return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new SimpleAnswer("not found order"));}
+
+        Product product = productReposytory.findById(UUID.fromString(jsLine.get("productId").textValue())).get();
+
+        if (product == null){return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new SimpleAnswer("not found product"));}
+
+        String characId = jsLine.get("characId").textValue();
+
+        Charac charac = null;
+
+        if (characId.length() > 0){
+            charac = characRepository.findById(UUID.fromString(characId)).get();
+
+            if (charac == null){return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new SimpleAnswer("not found charac"));}
+        }
+
+        int count = jsLine.get("count").intValue();
+        double price = jsLine.get("price").doubleValue();
+
+        order.addLine(product, charac, count, price);
+
+        orderRepository.save(order);
+
         return new ResponseEntity(HttpStatus.OK);
     }
 
     @PostMapping(value = "/changeCount", consumes = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity changeCount(@RequestBody JsonNode jsLine){
-//        long id = jsLine.get("id").longValue();
-//
-//        Order order = orderRepository.findById(id);
-//
-//        if (order == null){return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new SimpleAnswer("not found order"));}
-//
-//        int lineNumber = jsLine.get("lineNumber").intValue();
-//        int newCount = jsLine.get("newCount").intValue();
-//
-//        order.changeCount(lineNumber, newCount);
-//
-//        orderRepository.save(order);
+        String id = jsLine.get("id").textValue();
+
+        Order order = orderRepository.findById(UUID.fromString(id)).get();
+
+        if (order == null){return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new SimpleAnswer("not found order"));}
+
+        int lineNumber = jsLine.get("lineNumber").intValue();
+        int newCount = jsLine.get("newCount").intValue();
+
+        order.changeCount(lineNumber, newCount);
+
+        orderRepository.save(order);
 
         return new ResponseEntity(HttpStatus.OK);
     }
 
     @DeleteMapping(value = "/deleteLine")
     public ResponseEntity deleteLine(@RequestBody JsonNode jsLine){
-//        long id = jsLine.get("id").longValue();
-//
-//        Order order = orderRepository.findById(id);
-//
-//        if (order == null){return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new SimpleAnswer("not found order"));}
-//
-//        order.deleteLine(jsLine.get("lineNumber").intValue());
-//
-//        orderRepository.save(order);
-//        orderLineRepository.deleteAllByDeleted();
+        String id = jsLine.get("id").textValue();
+
+        Order order = orderRepository.findById(UUID.fromString(id)).get();
+
+        if (order == null){return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new SimpleAnswer("not found order"));}
+
+        order.deleteLine(jsLine.get("lineNumber").intValue());
+
+        orderRepository.save(order);
+        orderLineRepository.deleteAllByDeleted();
 
         return new ResponseEntity(HttpStatus.OK);
     }
 
     @DeleteMapping(value = "/deleteOrder")
     public ResponseEntity deleteOrder(@RequestBody JsonNode jsLine){
-//        Order order = orderRepository.findById(jsLine.get("id").longValue());
-//
-//        if (order == null) return new ResponseEntity(HttpStatus.NOT_FOUND);
-//
-//        if (order.getId1c() != null){
-//            if (!order.getId1c().isEmpty()) return new ResponseEntity("order follow to 1c, sorry", HttpStatus.CONFLICT);
-//        }
-//
-//        orderLineRepository.deleteAllByOrder(order);
-//        orderRepository.delete(order);
+        Order order = orderRepository.findById(UUID.fromString(jsLine.get("id").textValue())).get();
+
+        if (order == null) return new ResponseEntity(HttpStatus.NOT_FOUND);
+
+        if (order.getId1c() != null){
+            if (!order.getId1c().isEmpty()) return new ResponseEntity("order follow to 1c, sorry", HttpStatus.CONFLICT);
+        }
+
+        orderLineRepository.deleteAllByOrder(order);
+        orderRepository.delete(order);
 
         return new ResponseEntity(HttpStatus.OK);
     }
@@ -200,34 +203,34 @@ public class OrdersController {
         ObjectMapper mapper = new ObjectMapper();
 
         ArrayNode jsOrders = mapper.createArrayNode();
-//
-//        for (Order order: orders) {
-//
-//            String userId = order.getUser() == null ? "" : order.getUser().getId1c();
-//            String id1c = order.getId1c() == null ? "" : order.getId1c();
-//
-//            ObjectNode jsOrder = mapper.createObjectNode();
-//            jsOrder.put("id", order.getId());
-//            jsOrder.put("date", order.getDate().toString());
-//            jsOrder.put("user", userId);
-//            jsOrder.put("id1c", id1c);
-//
-//            List<OrderLine> lines = order.getLines();
-//            ArrayNode jsLines = mapper.createArrayNode();
-//
-//            for (OrderLine ol: lines) {
-//                ObjectNode jsLine = mapper.createObjectNode();
-//                jsLine.put("productId", ol.getProduct().getId1c());
-//                jsLine.put("characId", ol.getCharac().getId1c());
-//                jsLine.put("count", ol.getCount());
-//                jsLine.put("price", ol.getPrice());
-//                jsLine.put("sum", ol.getSum());
-//                jsLines.add(jsLine);
-//            }
-//
-//            jsOrder.put("lines", jsLines);
-//            jsOrders.add(jsOrder);
-//        }
+
+        for (Order order: orders) {
+
+            String userId = order.getUser() == null ? "" : order.getUser().getId1c();
+            String id1c = order.getId1c() == null ? "" : order.getId1c();
+
+            ObjectNode jsOrder = mapper.createObjectNode();
+            jsOrder.put("id", order.getId().toString());
+            jsOrder.put("date", order.getDate().toString());
+            jsOrder.put("user", userId);
+            jsOrder.put("id1c", id1c);
+
+            List<OrderLine> lines = order.getLines();
+            ArrayNode jsLines = mapper.createArrayNode();
+
+            for (OrderLine ol: lines) {
+                ObjectNode jsLine = mapper.createObjectNode();
+                jsLine.put("productId", ol.getProduct().getId1c());
+                jsLine.put("characId", ol.getCharac().getId1c());
+                jsLine.put("count", ol.getCount());
+                jsLine.put("price", ol.getPrice());
+                jsLine.put("sum", ol.getSum());
+                jsLines.add(jsLine);
+            }
+
+            jsOrder.put("lines", jsLines);
+            jsOrders.add(jsOrder);
+        }
 
         ObjectNode response = mapper.createObjectNode();
         return response.put("response", jsOrders);
@@ -253,32 +256,32 @@ public class OrdersController {
 
     @PostMapping(value = "/setUser")
     public ResponseEntity setUser(@RequestBody JsonNode jsLine){
-//        Order order = orderRepository.findById(jsLine.get("id").longValue());
-        //User user = userRepository.findById(jsLine.get("userId").intValue()).get();
+        Order order = orderRepository.findById(UUID.fromString(jsLine.get("id").textValue())).get();
+        User user = userRepository.findById(UUID.fromString(jsLine.get("userId").textValue())).get();
 
-//        if (order != null && user != null){
-//            order.setUser(user);
-//            orderRepository.save(order);
-//            return new ResponseEntity(HttpStatus.OK);
-//        }
+        if (order != null && user != null){
+            order.setUser(user);
+            orderRepository.save(order);
+            return new ResponseEntity(HttpStatus.OK);
+        }
 
         return new ResponseEntity(HttpStatus.NOT_FOUND);
     }
 
     @PostMapping(value = "/finalizeOrder")
     public ResponseEntity finalizeOrder(@RequestBody JsonNode jsonLine){
-//        Order order = orderRepository.findById(jsonLine.get("id").longValue());
-//
-//        if (order != null){
-//            if (order.getUser() == null)return ResponseEntity.status(HttpStatus.NOT_FOUND).body("user not set");
-//
-//            order.setStatus(OrderStatus.IN_WORK);
-//            order.setDateOpenOrder(order.getDate());
-//            order.setDate(LocalDateTime.now());
-//
-//            orderRepository.save(order);
-//            return new ResponseEntity(HttpStatus.OK);
-//        }
+        Order order = orderRepository.findById(UUID.fromString(jsonLine.get("id").textValue())).get();
+
+        if (order != null){
+            if (order.getUser() == null)return ResponseEntity.status(HttpStatus.NOT_FOUND).body("user not set");
+
+            order.setStatus(OrderStatus.IN_WORK);
+            order.setDateOpenOrder(order.getDate());
+            order.setDate(LocalDateTime.now());
+
+            orderRepository.save(order);
+            return new ResponseEntity(HttpStatus.OK);
+        }
 
         return new ResponseEntity(HttpStatus.NOT_FOUND);
     }
